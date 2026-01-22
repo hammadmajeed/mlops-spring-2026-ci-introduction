d6af899ebd0c9a0e6eb24f688e0c219296b0686c
hammadmajeed <hammadmajeed@gmail.com>
2026-01-22 12:42:07 +0500

    updated action code

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -5,7 +5,7 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(add(10, 5), 15)
-        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, 1), 1)
 
     def test_subtract(self):
         self.assertEqual(subtract(10, 5), 5)
