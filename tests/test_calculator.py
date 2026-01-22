01a38f6e1620406d19f48f84538583255aff1f54
hammadmajeed <hammadmajeed@gmail.com>
2026-01-22 10:47:51 +0500

    source code of calculator along with the test completed

---
+++ tests/test_calculator.py
@@ -0,0 +1,31 @@
+import unittest
+import src.calculator as add, subtract, multiply, divide
+import pytest
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(-1, -1) == -2
+    
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(-1, 1) == -2
+    assert subtract(-1, -1) == 0
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 1) == -1
+    assert multiply(-1, -1) == 1
+    
+def test_divide():
+    assert divide(6, 3) == 2
+    assert divide(-6, 2) == -3
+    assert divide(-6, -2) == 3
+    with pytest.raises(ValueError):
+        divide(1, 0)
+    
+if __name__ == '__main__':
+    test_add()
+    test_subtract()
+    test_multiply()
+    test_divide()
